1a1de2bfdacbd4d6ee8e96ea2a2405de3f1456fb
SbasLpz <sebastian.lopezluna5@gmail.com>
2024-11-19 17:12:49 -0600

    V1.3.0 Estable para Lanzamiento Android & IOS

---
+++ lib/Screens/MapWithStream/map_screen.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'map_screen.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$myUnitsListHash() => r'2113d1a3a0a8041d34efa1eaf45b78aa0f245b26';
+
+/// See also [myUnitsList].
+@ProviderFor(myUnitsList)
+final myUnitsListProvider =
+    AutoDisposeFutureProvider<List<UnidadModel>>.internal(
+  myUnitsList,
+  name: r'myUnitsListProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$myUnitsListHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef MyUnitsListRef = AutoDisposeFutureProviderRef<List<UnidadModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
